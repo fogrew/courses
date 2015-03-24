844ea4c777452097f7fb48678674b0aab65db181
Andrew <andrew@gurylev.com>
2015-03-24 07:55:48 +0300

    Начал страницу дизайна. Добавил все картинки, поменял текст в hero и базовый цвет. Переключил компилятор less2css на папку дизайна.

---
+++ design.php
@@ -0,0 +1,44 @@
+<?
+/*
+ ********************************
+ * Fogrew Landing Page CMS
+ * License: GNU
+ * Author:  Andrew Gurylev
+ * Site:    http://gurylev.com/
+ ********************************
+ */
+
+// define block in this page
+$blocks = [
+	'meta',
+	'header',
+	'hero',
+	'navbar',
+	'features',
+	'form-phone',
+	'results',
+	'who',
+	'form-mail',
+	'stats',
+	'photo',
+	'reviews',
+	'prices',
+	'shelude',
+	'program',
+	'faq',
+	'map',
+	'footer',
+	'similar',
+	'spoiler'
+];
+
+// setup configs
+require 'inc/config/index.php';
+
+$config['site']['view'] = 'design';
+$config['site']['text'] = 'design';
+
+// define globals variables
+require 'inc/core/engine/globals.php';
+// run blocks
+require 'inc/core/engine/index.php';
