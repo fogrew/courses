9effbf2783156fb6330f6436357c9e44baa74ff8
Andrew <andrew@gurylev.com>
2015-03-25 21:56:19 +0300

    Исправил оставшиеся массивы текстов

--- inc/text/ru/base/shelude.php
+++ inc/text/ru/base/shelude.php
@@ -1,107 +1,88 @@
-<?$text['shelude'] = [
-	'title'  => 'Расписание',
-	'courses' => [
-		[
-			'date'      => '2015-4-2',
-			'title'     => 'Начало 1 группы',
-			'date-text' => 'с 7 марта',
-			'time'      => 'по субботам 10:30—14:30',
-			'place'     => 'Кронверкский пр. д.9. Компьютерный класс 2-1',
-			'count'     => 'Осталось 8 мест!',
-			'button'    => 'Записаться',
-			'position'  => 'bottom'
-		],
-		[
-			'date'      => '2015-03-10',
-			'title'     => 'Начало 2 группы',
-			'date-text' => 'с 10 марта',
-			'time'      => 'по будням с 19:15 до 21:30',
-			'place'     => 'Кронверкский пр. д.9. Компьютерный класс 2-1',
-			'count'     => 'Осталось 2 места!',
-			'button'    => 'Записаться',
-			'position'  => 'top'
-		],
-		[
-			'date'      => '2015-03-23',
-			'title'     => 'Начало 3 группы',
-			'date-text' => 'с 23 марта',
-			'time'      => 'по будням с 19:15 до 21:30',
-			'place'     => 'Кронверкский пр. д.9. Компьютерный класс 2-1',
-			'count'     => 'Осталось 5 мест!',
-			'button'    => 'Записаться',
-			'position'  => 'right'
-		],
-		[
-			'date'      => '2015-03-27',
-			'title'     => 'Начало 4 группы',
-			'date-text' => 'с 27 марта',
-			'time'      => 'по будням с 19:15 до 21:30',
-			'place'     => 'Кронверкский пр. д.9. Компьютерный класс 2-1',
-			'count'     => 'Осталось 5 мест!',
-			'button'    => 'Записаться',
-			'position'  => 'top'
-		]
-	],
-	'months' => [
-		[
-			'short'  => 'янв.',
-			'full'   => 'Январь',
-			'plural' => 'января'
-		],
-		[
-			'short'  => 'февр.',
-			'full'   => 'Февраль',
-			'plural' => 'февраля'
-		],
-		[
-			'short'  => 'марта',
-			'full'   => 'Март',
-			'plural' => 'марта'
-		],
-		[
-			'short'  => 'апр.',
-			'full'   => 'Апрель',
-			'plural' => 'апреля'
-		],
-		[
-			'short'  => 'мая.',
-			'full'   => 'Май',
-			'plural' => 'мая'
-		],
-		[
-			'short'  => 'июня',
-			'full'   => 'Июнь',
-			'plural' => 'июня'
-		],
-		[
-			'short'  => 'июля',
-			'full'   => 'Июль',
-			'plural' => 'июля'
-		],
-		[
-			'short'  => 'авг.',
-			'full'   => 'Август',
-			'plural' => 'августа'
-		],
-		[
-			'short'  => 'сент.',
-			'full'   => 'Сентябрь',
-			'plural' => 'сентября'
-		],
-		[
-			'short'  => 'окт.',
-			'full'   => 'Октябрь',
-			'plural' => 'октября'
-		],
-		[
-			'short'  => 'нояб.',
-			'full'   => 'Ноябрь',
-			'plural' => 'ноября'
-		],
-		[
-			'short'  => 'дек.',
-			'full'   => 'Декабрь',
-			'plural' => 'декабря'
-		]
-	]
-];
+<?
+$text['shelude']['title']                   = 'Расписание';
+
+// Даты курсов
+$text['shelude']['courses'][0]['date']      = '2015-4-2';
+$text['shelude']['courses'][0]['title']     = 'Начало 1 группы';
+$text['shelude']['courses'][0]['date-text'] = 'с 7 марта';
+$text['shelude']['courses'][0]['time']      = 'по субботам 10:30—14:30';
+$text['shelude']['courses'][0]['place']     = 'Кронверкский пр. д.9. Компьютерный класс 2-1';
+$text['shelude']['courses'][0]['count']     = 'Осталось 8 мест!';
+$text['shelude']['courses'][0]['button']    = 'Записаться';
+$text['shelude']['courses'][0]['position']  = 'bottom';
+
+$text['shelude']['courses'][1]['date']      = '2015-03-10';
+$text['shelude']['courses'][1]['title']     = 'Начало 2 группы';
+$text['shelude']['courses'][1]['date-text'] = 'с 10 марта';
+$text['shelude']['courses'][1]['time']      = 'по будням с 19:15 до 21:30';
+$text['shelude']['courses'][1]['place']     = 'Кронверкский пр. д.9. Компьютерный класс 2-1';
+$text['shelude']['courses'][1]['count']     = 'Осталось 2 места!';
+$text['shelude']['courses'][1]['button']    = 'Записаться';
+$text['shelude']['courses'][1]['position']  = 'top';
+
+$text['shelude']['courses'][2]['date']      = '2015-03-23';
+$text['shelude']['courses'][2]['title']     = 'Начало 3 группы';
+$text['shelude']['courses'][2]['date-text'] = 'с 23 марта';
+$text['shelude']['courses'][2]['time']      = 'по будням с 19:15 до 21:30';
+$text['shelude']['courses'][2]['place']     = 'Кронверкский пр. д.9. Компьютерный класс 2-1';
+$text['shelude']['courses'][2]['count']     = 'Осталось 5 мест!';
+$text['shelude']['courses'][2]['button']    = 'Записаться';
+$text['shelude']['courses'][2]['position']  = 'right';
+
+$text['shelude']['courses'][3]['date']      = '2015-03-27';
+$text['shelude']['courses'][3]['title']     = 'Начало 4 группы';
+$text['shelude']['courses'][3]['date-text'] = 'с 27 марта';
+$text['shelude']['courses'][3]['time']      = 'по будням с 19:15 до 21:30';
+$text['shelude']['courses'][3]['place']     = 'Кронверкский пр. д.9. Компьютерный класс 2-1';
+$text['shelude']['courses'][3]['count']     = 'Осталось 5 мест!';
+$text['shelude']['courses'][3]['button']    = 'Записаться';
+$text['shelude']['courses'][3]['position']  = 'top';
+
+// Названия месяцев
+$text['shelude']['months'][0]['short']      = 'янв.';
+$text['shelude']['months'][0]['full']       = 'Январь';
+$text['shelude']['months'][0]['plural']     = 'января';
+
+$text['shelude']['months'][1]['short']      = 'февр.';
+$text['shelude']['months'][1]['full']       = 'Февраль';
+$text['shelude']['months'][1]['plural']     = 'февраля';
+
+$text['shelude']['months'][2]['short']      = 'марта';
+$text['shelude']['months'][2]['full']       = 'Март';
+$text['shelude']['months'][2]['plural']     = 'марта';
+
+$text['shelude']['months'][3]['short']      = 'апр.';
+$text['shelude']['months'][3]['full']       = 'Апрель';
+$text['shelude']['months'][3]['plural']     = 'апреля';
+
+$text['shelude']['months'][4]['short']      = 'мая';
+$text['shelude']['months'][4]['full']       = 'Май';
+$text['shelude']['months'][4]['plural']     = 'мая';
+
+$text['shelude']['months'][5]['short']      = 'июня';
+$text['shelude']['months'][5]['full']       = 'Июнь';
+$text['shelude']['months'][5]['plural']     = 'июня';
+
+$text['shelude']['months'][6]['short']      = 'июля';
+$text['shelude']['months'][6]['full']       = 'Июль';
+$text['shelude']['months'][6]['plural']     = 'июля';
+
+$text['shelude']['months'][7]['short']      = 'авг.';
+$text['shelude']['months'][7]['full']       = 'Август';
+$text['shelude']['months'][7]['plural']     = 'августа';
+
+$text['shelude']['months'][8]['short']      = 'сент.';
+$text['shelude']['months'][8]['full']       = 'Сентябрь';
+$text['shelude']['months'][8]['plural']     = 'сентября';
+
+$text['shelude']['months'][9]['short']      = 'окт.';
+$text['shelude']['months'][9]['full']       = 'Октябрь';
+$text['shelude']['months'][9]['plural']     = 'октября';
+
+$text['shelude']['months'][10]['short']      = 'нояб.';
+$text['shelude']['months'][10]['full']       = 'Ноябрь';
+$text['shelude']['months'][10]['plural']     = 'ноября';
+
+$text['shelude']['months'][11]['short']      = 'дек.';
+$text['shelude']['months'][11]['full']       = 'Декабрь';
+$text['shelude']['months'][11]['plural']     = 'декабря';
--- inc/text/ru/base/stats.php
+++ inc/text/ru/base/stats.php
@@ -1,25 +1,18 @@
-<?$text['stats'] = [
-	'title' => 'Статистика',
-	'media' => [
-		[
-			'image' => 'i/stats/1.png',
-			'title' => '392',
-			'text'  => 'человек, обученных в группах'
-		],
-		[
-			'image' => 'i/stats/2.png',
-			'title' => '64',
-			'text'  => 'человека, обученных индивидуально'
-		],
-		[
-			'image' => 'i/stats/3.png',
-			'title' => '2090',
-			'text'  => 'часов, потраченных на обучение'
-		],
-		[
-			'image' => 'i/stats/4.png',
-			'title' => '346',
-			'text'  => 'слушателей рекомендовали друзьям'
-		]
-	]
-];
+<?
+$text['stats']['title']             = 'Статистика';
+
+$text['stats']['media'][0]['image'] = 'i/stats/1.png';
+$text['stats']['media'][0]['title'] = '392';
+$text['stats']['media'][0]['text']  = 'человек, обученных в группах';
+
+$text['stats']['media'][1]['image'] = 'i/stats/2.png';
+$text['stats']['media'][1]['title'] = '64';
+$text['stats']['media'][1]['text']  = 'человека, обученных индивидуально';
+
+$text['stats']['media'][2]['image'] = 'i/stats/3.png';
+$text['stats']['media'][2]['title'] = '2090';
+$text['stats']['media'][2]['text']  = 'часов, потраченных на обучение';
+
+$text['stats']['media'][3]['image'] = 'i/stats/4.png';
+$text['stats']['media'][3]['title'] = '346';
+$text['stats']['media'][3]['text']  = 'слушателей рекомендовали друзьям';
