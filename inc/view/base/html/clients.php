02db3036898498db8a0ddf8ab827d689b9da1fef
Andrew <andrew@gurylev.com>
2015-03-27 23:50:59 +0300

    В блоке "Клиенты" теперь можно указать alt и title для логотипов

--- inc/view/base/html/clients.php
+++ inc/view/base/html/clients.php
@@ -4,7 +4,7 @@
     	<div class="title"><?=$text['clients']['title']?></div>
       <div class="owl-carousel">
 <?foreach ($text['clients']['content'] as $i => $client) { ?>
-        <img class="owl-lazy" data-src="<?=$client['image']?>" alt="">
+        <img class="owl-lazy" data-src="<?=$client['image']?>" alt="<?=$client['alt']?>" title="<?=$client['title']?>">
 <?}?>
       </div>
     </div>
