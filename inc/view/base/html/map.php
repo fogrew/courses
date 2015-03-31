7ae6635e54e6d6b59a90cdeb05989e579a482e14
Andrew <andrew@gurylev.com>
2015-03-31 08:05:33 +0300

    Разметили overlay над картой микроразметкой

--- inc/view/base/html/map.php
+++ inc/view/base/html/map.php
@@ -1,8 +1,14 @@
 
   <div class="map">
-  	<div class="map__overlay">
-  		<div class="map__title"><?=$text['map']['title']?></div>
-  		<div class="map__text"><?=$text['map']['text']?></div>
+  	<div class="map__overlay" itemscope itemtype="http://schema.org/Organization">
+  		<div class="map__title" itemprop="name"><?=$text['map']['title']?></div>
+  		<span class="map__text" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+  			<span itemprop="addressLocality"><?=$text['map']['address']?></span>
+  			<span itemprop="streetAddress"><?=$text['map']['street']?></span>
+			</span>
+			<span class="map__contant">
+				<span itemprop="telephone"><?=$text['map']['phone']?></span>, <span itemprop="email"><?=$text['map']['email']?></span>
+			</span>
   	</div>
     <div id="map"></div>
   </div>
