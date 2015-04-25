834aca5e2c7283705b7d78a0ae72c42acb4f185d
Andrew Gurylev <andrew@gurylev.com>
2015-04-26 01:40:39 +0300

    fix: имя ссылки на последнюю форму конфликтовало с колбэк хантером. теперь ссылка не на #callback, а на #footer

--- inc/view/base/html/header.php
+++ inc/view/base/html/header.php
@@ -1,7 +1,7 @@
       <div class="header__feedback feedback">
         <div class="feedback__title"><?=$text['header']['contact']?></div>
         <div class="feedback__phone"><?=$text['header']['phone']?></div>
-        <a href="#callback" class="feedback__action button"><?=$text['header']['callback']?></a>
+        <a href="#footer" class="feedback__action button"><?=$text['header']['callback']?></a>
       </div>
       <img src="<?=$text['header']['logo']?>" alt="<?=$text['header']['line1']?>" class="header__logo">
       <div class="header__title">
