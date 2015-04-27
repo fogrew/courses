3cd9c1d42e2900ce0da2465c47452a4eee540fd1
Andrew Gurylev <andrew@gurylev.com>
2015-04-27 08:13:16 +0300

    Добавил скрипты с паблика (ужас!)

--- inc/view/base/html/scripts.php
+++ inc/view/base/html/scripts.php
@@ -6,5 +6,13 @@
   <script src="js/main.js"></script>
   <script type="text/javascript" src="//cdn.callbackhunter.com/widget/tracker.js" charset="UTF-8"></script>
   <script type="text/javascript">var hunter_code="33aedfa723ce0c76c0c7ad567ccb62d0";</script>
+  <!-- Yandex.Metrika counter --><script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter24847253 = new Ya.Metrika({id:24847253, webvisor:true, clickmap:true, trackLinks:true, accurateTrackBounce:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/24847253" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->	
+	<!--Ucall.im-->
+	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script> var jQ = jQuery.noConflict();</script>
+	<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<a id="callback" data-token="a50683a1bf6a8d63954caea69e4ab66e" href="#ucall" style="display:none;">Обратный звонок</a>
+	<script src="//ucall.im/callback/js/callback.js" type="text/javascript"></script>
+	<!--Ucall.im-->
 </body>
 </html>
